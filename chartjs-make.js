822200a700026854689003ca94392ee9197ac05f
Snorre Foss Westman <snorrefo@gmail.com>
2018-01-14 19:05:46 +0100

    Initial commit

---
+++ chartjs-make.js
@@ -0,0 +1,53 @@
+// // Bar chart
+// new Chart(document.getElementById("myChart"), {
+//     type: 'bar',
+//     data: {
+//       labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+//       datasets: [
+//         {
+//           label: "Population (millions)",
+//           backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+//           data: [2478,5267,734,784,433]
+//         }
+//       ]
+//     },
+//     options: {
+//       legend: { display: false },
+//       title: {
+//         display: true,
+//         text: 'Predicted world population (millions) in 2050'
+//       }
+//     }
+// });
+
+var ctx = document.getElementById("myChart").getContext("2d");
+var scatterChart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'Scatter Dataset',
+            backgroundColor: ["#3e95cd"],
+            data: [{
+                x: -10,
+                y: 0
+            }, {
+                x: 0,
+                y: 10
+            }, {
+                x: 10,
+                y: 5
+            }, {
+                x: 1,
+                y: 3
+            }]
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        }
+    }
+});
